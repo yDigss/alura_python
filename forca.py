65b80bffd8ad8d8efe26a815f39db116a27e83c2
Rodrigo Silva <rodrigo.7762018@gmail.com>
2025-10-20 14:16:57 -0300

    feat (hangman): implement core gameplay mechanics

--- forca.py
+++ forca.py
@@ -4,6 +4,40 @@ def jogar_forca():
     print("Bem-Vindo ao jogo de Forca!")
     print("*"*28)
 
+    palavra_secreta = "banana".lower()
+    letras_acertadas = ["_" for letra in palavra_secreta]
+
+    enforcou = False
+    acertou = False
+    erros = 0
+
+    print(letras_acertadas)
+
+    while not enforcou and not acertou:
+
+        chute = input("Qual letra? ").strip().lower()
+
+        if chute in palavra_secreta:
+            i = 0
+            for letra in palavra_secreta:
+                if chute == letra:
+                    letras_acertadas[i] = letra
+                i += 1
+
+        else:
+            erros += 1
+
+        enforcou = erros == 6
+        acertou = "_" not in letras_acertadas
+
+        print(letras_acertadas)
+
+    if acertou:
+        print("Você Ganhou!!")
+
+    else:
+        print("Você Perdeu!")
+
     print("Fim do Jogo!")
 
 if __name__ == "__main__":
